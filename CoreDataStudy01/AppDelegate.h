46004a65d2bc0ca53f0832872a6c0c4e6856ae03
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2014-04-26 08:17:23 +0900

    Initial Commit

---
+++ CoreDataStudy01/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  CoreDataStudy01
+//
+//  Created by 布川祐人 on 2014/04/26.
+//  Copyright (c) 2014年 NUNOKAWA Masato. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
